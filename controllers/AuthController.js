638c35d8930dc4b66d4a131d4af387d47443fade
AsGhrK <asghrkcasari@gmail.com>
2024-11-05 09:48:16 -0400

    05-11-24
    feita o cadastro de mesas e organização do codigo

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -3,290 +3,137 @@ const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 class AuthController {
-  // Método estático para cadastro de novos usuários
+  // Método para cadastro de novos usuários
   static async cadastro(req, res) {
     const { nome, email, password } = req.body;
 
-    // Validação do nome
-    if (!nome || nome.length < 6) {
-      return res.status(422).json({
-        erro: true,
-        mensagem: "O nome deve ter pelo menos 6 caracteres.",
-      });
-    }
+    // Validações de entrada
+    if (!nome || nome.length < 6) 
+      return res.status(422).json({ erro: true, mensagem: "O nome deve ter pelo menos 6 caracteres." });
+    if (!email || email.length < 10) 
+      return res.status(422).json({ erro: true, mensagem: "O email deve ter pelo menos 10 caracteres." });
+    if (!password || password.length < 8) 
+      return res.status(422).json({ erro: true, mensagem: "A senha deve ter pelo menos 8 caracteres." });
 
-    // Validação do email
-    if (!email || email.length < 10) {
-      return res.status(422).json({
-        erro: true,
-        mensagem: "O email deve ter pelo menos 10 caracteres.",
-      });
-    }
+    // Verifica se o usuário já existe
+    const existe = await prisma.usuario.count({ where: { email } });
+    if (existe) 
+      return res.status(422).json({ erro: true, mensagem: "Já existe um usuário com este e-mail" });
 
-    // Validação da senha
-    if (!password || password.length < 8) {
-      return res.status(422).json({
-        erro: true,
-        mensagem: "A senha deve ter pelo menos 8 caracteres.",
-      });
-    }
-
-    // Verifica se já existe um usuário com o mesmo e-mail
-    const existe = await prisma.usuario.count({
-      where: { email: email },
-    });
-
-    if (existe != 0) {
-      return res.status(422).json({
-        erro: true,
-        mensagem: "Já existe um usuário com este e-mail",
-      });
-    }
-
-    // Gera um salt e hash da senha
-    const salt = bcryptjs.genSaltSync(8);
-    const hashPassword = bcryptjs.hashSync(password, salt);
-
+    // Cria usuário com hash de senha
+    const hashPassword = bcryptjs.hashSync(password, bcryptjs.genSaltSync(8));
     try {
-      // Cria um novo usuário no banco de dados
-      const usuario = await prisma.usuario.create({
-        data: {
-          nome: nome,
-          email: email,
-          password: hashPassword,
-          tipo: "cliente",
-        },
+      await prisma.usuario.create({
+        data: { nome, email, password: hashPassword, tipo: "cliente" }
       });
-
-      // Resposta de sucesso
-      return res.status(201).json({
-        erro: false,
-        mensagem: "Usuário cadastrado com sucesso!",
-      });
+      return res.status(201).json({ erro: false, mensagem: "Usuário cadastrado com sucesso!" });
     } catch (error) {
-      // Resposta em caso de erro
-      return res.status(500).json({
-        erro: true,
-        mensagem: "Ocorreu um erro, tente novamente mais tarde! " + error,
-      });
+      return res.status(500).json({ erro: true, mensagem: "Ocorreu um erro, tente novamente mais tarde! " + error });
     }
   }
 
-  // Método estático para login de usuários
+  // Método para login de usuários
   static async login(req, res) {
     const { email, password } = req.body;
 
-    // Busca o usuário pelo e-mail
-    const usuario = await prisma.usuario.findUnique({
-      where: { email: email },
-    });
+    const usuario = await prisma.usuario.findUnique({ where: { email } });
+    if (!usuario) 
+      return res.status(422).json({ erro: true, mensagem: "Usuário não encontrado." });
 
-    // Verifica se o usuário existe
-    if (!usuario) {
-      return res.status(422).json({
-        erro: true,
-        mensagem: "Usuário não encontrado.",
-      });
-    }
+    if (!bcryptjs.compareSync(password, usuario.password)) 
+      return res.status(422).json({ erro: true, mensagem: "Senha incorreta." });
 
-    // Verifica a senha usando bcryptjs
-    const senhaCorreta = bcryptjs.compareSync(password, usuario.password);
-
-    if (!senhaCorreta) {
-      return res.status(422).json({
-        erro: true,
-        mensagem: "Senha incorreta.",
-      });
-    }
-
-    // Gera um token JWT para o usuário
-    const token = jwt.sign({ id: usuario.id }, process.env.SECRET_KEY, {
-      expiresIn: "1h",
-    });
-
-    // Resposta de sucesso com o token
-    res.status(200).json({
-      erro: false,
-      mensagem: "Autenticação realizada com sucesso!",
-      token: token,
-    });
+    const token = jwt.sign({ id: usuario.id }, process.env.SECRET_KEY, { expiresIn: "1h" });
+    res.status(200).json({ erro: false, mensagem: "Autenticação realizada com sucesso!", token });
   }
 
-  // Método estático para cadastro de novos administradores
+  // Método para cadastro de novos administradores
   static async cadastroAdm(req, res) {
     const { nome, email, password } = req.body;
 
-    // Validação do nome
-    if (!nome || nome.length < 6) {
-      return res.status(422).json({
-        erro: true,
-        mensagem: "O nome deve ter pelo menos 6 caracteres.",
-      });
-    }
+    if (!nome || nome.length < 6) 
+      return res.status(422).json({ erro: true, mensagem: "O nome deve ter pelo menos 6 caracteres." });
+    if (!email || email.length < 10) 
+      return res.status(422).json({ erro: true, mensagem: "O email deve ter pelo menos 10 caracteres." });
+    if (!password || password.length < 8) 
+      return res.status(422).json({ erro: true, mensagem: "A senha deve ter pelo menos 8 caracteres." });
 
-    // Validação do email
-    if (!email || email.length < 10) {
-      return res.status(422).json({
-        erro: true,
-        mensagem: "O email deve ter pelo menos 10 caracteres.",
-      });
-    }
+    const existe = await prisma.usuarioAdm.count({ where: { email } });
+    if (existe) 
+      return res.status(422).json({ erro: true, mensagem: "Já existe um usuário com este e-mail" });
 
-    // Validação da senha
-    if (!password || password.length < 8) {
-      return res.status(422).json({
-        erro: true,
-        mensagem: "A senha deve ter pelo menos 8 caracteres.",
-      });
-    }
-
-    // Verifica se já existe um usuário com o mesmo e-mail
-    const existe = await prisma.usuarioAdm.count({
-      where: { email: email },
-    });
-
-    if (existe != 0) {
-      return res.status(422).json({
-        erro: true,
-        mensagem: "Já existe um usuário com este e-mail",
-      });
-    }
-
-    // Gera um salt e hash da senha
-    const salt = bcryptjs.genSaltSync(8);
-    const hashPassword = bcryptjs.hashSync(password, salt);
-
+    const hashPassword = bcryptjs.hashSync(password, bcryptjs.genSaltSync(8));
     try {
-      // Cria um novo administrador no banco de dados
-      const usuarioAdm = await prisma.usuarioAdm.create({
-        data: {
-          nome: nome,
-          email: email,
-          password: hashPassword,
-          tipo: "Adm",
-        },
+      await prisma.usuarioAdm.create({
+        data: { nome, email, password: hashPassword, tipo: "Adm" }
       });
-
-      // Resposta de sucesso
-      return res.status(201).json({
-        erro: false,
-        mensagem: "Administrador cadastrado com sucesso!",
-      });
+      return res.status(201).json({ erro: false, mensagem: "Administrador cadastrado com sucesso!" });
     } catch (error) {
-      // Resposta em caso de erro
-      return res.status(500).json({
-        erro: true,
-        mensagem: "Ocorreu um erro, tente novamente mais tarde! " + error,
-      });
+      return res.status(500).json({ erro: true, mensagem: "Ocorreu um erro, tente novamente mais tarde! " + error });
     }
   }
 
-  // Método estático para login de administradores
+  // Método para login de administradores
   static async loginAdm(req, res) {
     const { email, password } = req.body;
 
-    // Busca o administrador pelo e-mail
-    const usuarioAdm = await prisma.usuarioAdm.findUnique({
-      where: { email: email },
-    });
+    const usuarioAdm = await prisma.usuarioAdm.findUnique({ where: { email } });
+    if (!usuarioAdm) 
+      return res.status(422).json({ erro: true, mensagem: "Administrador não encontrado." });
 
-    // Verifica se o administrador existe
-    if (!usuarioAdm) {
-      return res.status(422).json({
-        erro: true,
-        mensagem: "Administrador não encontrado.",
-      });
-    }
+    if (!bcryptjs.compareSync(password, usuarioAdm.password)) 
+      return res.status(422).json({ erro: true, mensagem: "Senha incorreta." });
 
-    // Verifica a senha usando bcryptjs
-    const senhaCorreta = bcryptjs.compareSync(password, usuarioAdm.password);
-
-    if (!senhaCorreta) {
-      return res.status(422).json({
-        erro: true,
-        mensagem: "Senha incorreta.",
-      });
-    }
-
-    // Gera um token JWT para o administrador
-    const token = jwt.sign({ id: usuarioAdm.id }, process.env.SECRET_KEY, {
-      expiresIn: "1h",
-    });
-
-    // Resposta de sucesso com o token
-    res.status(200).json({
-      erro: false,
-      mensagem: "Autenticação realizada com sucesso!",
-      token: token,
-    });
+    const token = jwt.sign({ id: usuarioAdm.id }, process.env.SECRET_KEY, { expiresIn: "1h" });
+    res.status(200).json({ erro: false, mensagem: "Autenticação realizada com sucesso!", token });
   }
 
+  // Método para cadastro de mesas
+  static async cadastroMesa(req, res) {
+    const { codigo, n_pessoas } = req.body;
+
+    if (!codigo) 
+      return res.status(422).json({ erro: true, mensagem: "Codigo já existente." });
+    if (!n_pessoas || isNaN(n_pessoas) || n_pessoas <= 0) 
+      return res.status(422).json({ erro: true, mensagem: "O número de pessoas deve ser um valor positivo." });
+
+    const existe = await prisma.CriaMesa.count({ where: { codigo } });
+    if (existe) 
+      return res.status(422).json({ erro: true, mensagem: "Já existe uma mesa com este código." });
+
+    try {
+      const criaMesa = await prisma.criaMesa.create({ data: { codigo, n_pessoas, tipo: "Mesa" } });
+      const token = jwt.sign({ id: criaMesa.id }, process.env.SECRET_KEY, { expiresIn: "1h" });
+      return res.status(201).json({ erro: false, mensagem: "Mesa cadastrada com sucesso!", token });
+    } catch (error) {
+      return res.status(500).json({ erro: true, mensagem: "Ocorreu um erro ao efetuar sua reserva, tente novamente mais tarde! " + error.message });
+    }
+  }
+
+  // Método para cadastro de reservas
   static async cadastroReservas(req, res) {
     const { nome_reserva, data_reserva, n_pessoas } = req.body;
-  
-    // Validação do nome_reserva
-    if (!nome_reserva || nome_reserva.length < 3) {
-      return res.status(422).json({
-        erro: true,
-        mensagem: "O nome da reserva deve ter pelo menos 3 caracteres.",
-      });
-    }
-  
-    // Validação de n_pessoas
-    if (!n_pessoas || isNaN(n_pessoas) || n_pessoas <= 0) {
-      return res.status(422).json({
-        erro: true,
-        mensagem: "O número de pessoas deve ser um valor positivo.",
-      });
-    }
-  
-    // Verifica se a data_reserva é válida
+
+    if (!nome_reserva || nome_reserva.length < 3) 
+      return res.status(422).json({ erro: true, mensagem: "O nome da reserva deve ter pelo menos 3 caracteres." });
+    if (!n_pessoas || isNaN(n_pessoas) || n_pessoas <= 0) 
+      return res.status(422).json({ erro: true, mensagem: "O número de pessoas deve ser um valor positivo." });
+
     const dataReserva = new Date(data_reserva);
-    if (isNaN(dataReserva.getTime())) {
-      return res.status(422).json({
-        erro: true,
-        mensagem: "A data da reserva deve ser uma data válida no formato ISO.",
-      });
-    }
-  
-    // Verifica se já existe uma reserva com o mesmo nome_reserva
-    const existe = await prisma.reservaMesas.count({
-      where: { nome_reserva: nome_reserva },
-    });
-  
-    if (existe !== 0) {
-      return res.status(422).json({
-        erro: true,
-        mensagem: "Já existe uma reserva com este nome.",
-      });
-    }
-  
+    if (isNaN(dataReserva.getTime())) 
+      return res.status(422).json({ erro: true, mensagem: "A data da reserva deve ser uma data válida no formato ISO." });
+
+    const existe = await prisma.reservaMesas.count({ where: { nome_reserva } });
+    if (existe) 
+      return res.status(422).json({ erro: true, mensagem: "Já existe uma reserva com este nome." });
+
     try {
-      // Cria uma nova reserva no banco de dados
-      const reservaMesas = await prisma.reservaMesas.create({
-        data: {
-          nome_reserva: nome_reserva,
-          data_reserva: dataReserva, // Usa o objeto Date convertido
-          n_pessoas: n_pessoas,
-          tipo: "Reserva",
-        },
-      });
-  
-      // Resposta de sucesso
-      return res.status(201).json({
-        erro: false,
-        mensagem: "Reserva cadastrada com sucesso!",
-      });
+      await prisma.reservaMesas.create({ data: { nome_reserva, data_reserva: dataReserva, n_pessoas, tipo: "Reserva" } });
+      return res.status(201).json({ erro: false, mensagem: "Reserva cadastrada com sucesso!" });
     } catch (error) {
-      // Resposta em caso de erro
-      return res.status(500).json({
-        erro: true,
-        mensagem: "Ocorreu um erro ao efetuar sua reserva, tente novamente mais tarde! " + error.message,
-      });
+      return res.status(500).json({ erro: true, mensagem: "Ocorreu um erro ao efetuar sua reserva, tente novamente mais tarde! " + error.message });
     }
   }
-  
 }
 
-
-module.exports = AuthController; // Exporta o controlador para ser utilizado em outras partes da aplicação.
+module.exports = AuthController;
