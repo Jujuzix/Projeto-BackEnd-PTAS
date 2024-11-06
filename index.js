dd5a79e868ed1b60f360bef60a52580d2d8b0d41
Davi Casari <131817273+AsGhrK@users.noreply.github.com>
2024-11-06 19:19:46 -0400

    index

---
+++ index.js
@@ -0,0 +1,16 @@
+const prisma = require("./prisma/prismaClient");
+
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+app.use(express.urlencoded({extended: true}));
+
+//rotas
+const authRoutes = require("./routes/authRoute");
+app.use("/auth", authRoutes); 
+const mesaRoutes = require("./routes/mesaRoute");
+app.use("/mesa", mesaRoutes); 
+
+app.listen(8000);
