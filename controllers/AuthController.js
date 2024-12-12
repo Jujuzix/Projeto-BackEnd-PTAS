1a2e0a224ccfca2833d90377ce4016d59100b7a6
AsGhrK <asghrkcasari@gmail.com>
2024-12-12 10:34:54 -0400

    12-12-2024

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -47,46 +47,26 @@ class AuthController {
     res.status(200).json({ erro: false, mensagem: "Autenticação realizada com sucesso!", token });
   }
 
-  // Método para cadastro de novos administradores
-   static async cadastroAdm(req, res) {
-     const { nome, email, password } = req.body;
- 
-     if (!nome || nome.length < 6) 
-       return res.status(422).json({ erro: true, mensagem: "O nome deve ter pelo menos 6 caracteres." });
-     if (!email || email.length < 10) 
-       return res.status(422).json({ erro: true, mensagem: "O email deve ter pelo menos 10 caracteres." });
-     if (!password || password.length < 8) 
-       return res.status(422).json({ erro: true, mensagem: "A senha deve ter pelo menos 8 caracteres." });
- 
-     const existe = await prisma.usuarioAdm.count({ where: { email } });
-     if (existe) 
-       return res.status(422).json({ erro: true, mensagem: "Já existe um usuário com este e-mail" });
- 
-     const hashPassword = bcryptjs.hashSync(password, bcryptjs.genSaltSync(8));
-     try {
-       await prisma.usuarioAdm.create({
-         data: { nome, email, password: hashPassword, tipo: "Adm" }
-       });
-       return res.status(201).json({ erro: false, mensagem: "Administrador cadastrado com sucesso!" });
-     } catch (error) {
-       return res.status(500).json({ erro: true, mensagem: "Ocorreu um erro, tente novamente mais tarde! " + error });
-     }
-   }
- 
-    // Método para login de administradores
-   static async loginAdm(req, res) {
-     const { email, password } = req.body;
- 
-     const usuarioAdm = await prisma.usuarioAdm.findUnique({ where: { email } });
-     if (!usuarioAdm) 
-       return res.status(422).json({ erro: true, mensagem: "Administrador não encontrado." });
- 
-     if (!bcryptjs.compareSync(password, usuarioAdm.password)) 
-       return res.status(422).json({ erro: true, mensagem: "Senha incorreta." });
- 
-     const token = jwt.sign({ id: usuarioAdm.id }, process.env.SECRET_KEY, { expiresIn: "1h" });
-     res.status(200).json({ erro: false, mensagem: "Autenticação realizada com sucesso!", token });
-   }
+static async verficarAutenticacao(req, res, next){
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if(!token){
+    return res.status(422).json({messsage: "TOken não encontrado"})
+  }
+
+  jwt.verify(token, process.env.SECRET_KEY, (err, payload) =>{
+    if(err){
+      return res.status(401).json({msg: "Token invalido"})
+    }
+
+    req.usuarioId = payload.id;
+    next()
+  });
+
 }
+
+}
+
 
 module.exports = AuthController;
--- index.js
+++ index.js
@@ -3,18 +3,28 @@ const prisma = require("./prisma/prismaClient");
 
 const express = require("express");
 
+const AuthController = require ("./controllers/AuthController")
+
 const app = express();
 app.use(cors({
-    origin:"http://localhost:3000"
+    origin: "http://localhost:3000"
 }))
 app.use(express.json());
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 //rotas
 const authRoutes = require("./routes/authRoute");
-app.use("/auth", authRoutes); 
+app.use("/auth", authRoutes);
 const mesaRoutes = require("./routes/mesaRoute");
-app.use("/mesas", mesaRoutes); 
+app.use("/mesas", mesaRoutes);
+
+app.use("/perfil" , AuthController.verficarAutenticacao, perfilRoutes);
+
+app.get("/privado", AuthController.verficarAutenticacao, (req, res) => {
+    res.json({
+        msg: "Voce entrou em area privada"
+    });
+});
 
 app.listen(8000);
