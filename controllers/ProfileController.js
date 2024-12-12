6b944717862062333487844dd226ba8e32d01536
AsGhrK <asghrkcasari@gmail.com>
2024-12-12 10:42:31 -0400

    12-12-24

---
+++ controllers/ProfileController.js
@@ -0,0 +1,9 @@
+const prisma = require("../prisma/prismaClient")
+
+class ProfileController{
+    static async visualizar(req, res){
+        const usuario = await prisma.usuario.findUnique({
+            where: {id: req.usuarioId},
+    })
+    }
+}
